a8ef4ac13b36f2647b93f301a20b63d70889fa50
Liz Manresa <manresa.liz@gmail.com>
2023-06-13 23:03:40 +0200

    quest done

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace S04_dictionnaire;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dictionary<char, string> dictionnaire = new Dictionary<char, string>();
+        dictionnaire['a'] = "abandon";
+        dictionnaire['b'] = "ballad";
+        dictionnaire['c'] = "cake";
+
+        //avant suppression
+        foreach (KeyValuePair<char, string> paire in dictionnaire)
+        {
+            Console.WriteLine("Lettre : " + paire.Key + ", Mot : " + paire.Value);
+        }
+
+        //suppression de la lettre c
+        dictionnaire.Remove('c');
+
+        // après suppression
+        foreach (KeyValuePair<char, string> paire in dictionnaire)
+        {
+            Console.WriteLine("Lettre : " + paire.Key + ", Mot : " + paire.Value);
+        }
+
+    }
+}
